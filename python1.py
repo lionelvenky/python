8db4c51579050839c23e21714421714b9e6a3e86
lionelvenky <143313703+lionelvenky@users.noreply.github.com>
2023-11-15 11:41:19 +0530

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,17 @@
+a= 10; #Integer Datatype
+b= 11.5; #Float Datatypo
+c=.05j; #Complex Number
+print ("a is type of",type(a));
+print("b is Type of", type(b));
+print("c is Type of",type(c));
+
+
+a=int(input("Enter a value"));
+b= int(input("Enter b value")); 
+print("Addition of a and b ",a+b);
+print("Subtraction of a and b ", a-b);
+print("Multiplication of a and b ",a *b);
+print("Division of a and b ", a/b);
+print("Remainder of a and b ", a%b);
+print("Exponent of a and bI", a**b);
+print("Floar division of a and b ", a//b);
