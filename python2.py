8dfe188ce59df69beff346f85bb1f889a1a82bcb
lionelvenky <143313703+lionelvenky@users.noreply.github.com>
2023-11-16 10:35:53 +0530

    Add files via upload

---
+++ python2.py
@@ -0,0 +1,25 @@
+
+# concatenate
+
+
+s1=input("Enter first String ");
+s2=input("Enter second String");
+print ("First string is : ",s1);
+print ("Second string is :", s2);
+print ("concatenations of two strings :",s1+s2);
+print ("Substring of given string :" ,s1 [1:4]);
+
+
+
+
+
+#append and remove
+
+pets = ['eat', 'dog', 'rat', 'pig',"tiger"]
+snakes= ['python', 'anaconda', 'fish' 'cobra' 'mamba']
+print("Pets are :", pets)
+print("Snakes are ;" ,snakes)
+animals= pets+snakes
+print("Animals are :", animals)
+snakes.remove ("fish")
+print("updated Snakes are: ", snakes)
