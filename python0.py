7d355f20effa71dfe7f2a50fba90cfdbdc3d8033
lionelvenky <143313703+lionelvenky@users.noreply.github.com>
2023-11-20 10:20:52 +0530

    Add files via upload

---
+++ python0.py
@@ -0,0 +1,21 @@
+# variables and datatypes:
+
+name = "venky"
+age = 25
+is_student = False
+
+
+# control flow ;
+
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
+
+
+
+# function:
+
+def greet(name):
+    print("Hello, " + name + "!")
+greet("venky")
