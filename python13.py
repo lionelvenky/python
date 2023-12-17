f6b1186d3a56fc20d12d4d370d8745daf4bd6391
lionelvenky <143313703+lionelvenky@users.noreply.github.com>
2023-12-17 22:09:00 +0530

    Add files via upload

---
+++ python13.py
@@ -0,0 +1,34 @@
+#control flow
+
+
+score = float(input("Enter your exam score: "))
+
+if 90 <= score <= 100:
+    grade = "A"
+elif 80 <= score < 90:
+    grade = "B"
+elif 70 <= score < 80:
+    grade = "C"
+elif 60 <= score < 70:
+    grade = "D"
+else:
+    grade = "F"
+
+print(f"Your grade is: {grade}")
+
+
+
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+number = int(input("Enter a number: "))
+
+if is_prime(number):
+    print(f"{number} is a prime number.")
+else:
+    print(f"{number} is not a prime number.")
